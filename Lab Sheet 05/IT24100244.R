b84bbccefd04bb13f0577e9bc02579bef1bfec15
IT24100244 <it24100244@my.sliit.lk>
2025-08-26 22:25:45 +0530

    Delete Lab Sheet 05/IT24100244.R

--- Lab Sheet 05/IT24100244.R
+++
@@ -1,7 +0,0 @@
-getwd()
-setwd("C:\\Users\\IT24100244\\Desktop\\IT24100244")
-getwd()
-
-Delivery_Times<-read.table("Exercise – Lab 05.txt",header = TRUE,sep=",")
-Delivery_Times
-fix(Delivery_Times)
